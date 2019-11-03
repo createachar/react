951d439e0592d60ae6e2e2437d5293e7b8b9aa54
username <user@email.mail>
2019-11-03 20:11:43 +0900

    Firebase font fail

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './index.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+const theme = createMuiTheme({// 테마를 명시함
+    typography: {
+        useNextVariants: true,
+        fontFamily: "Noto Sans KR"
+    }
+});
+
+
+ReactDOM.render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider>, document.getElementById('app'));
 
