027f6b48c27101637b16227a79497a8afd40bed0
username <user@email.mail>
2019-11-03 14:32:07 +0900

    Add Router Module

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -1,4 +1,7 @@
+
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Drawer from '@material-ui/core/Drawer';
@@ -6,37 +9,56 @@ import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-const styles={
-    root:{
+const styles = {
+    root: {
         flexGrow: 1,
     },
     menuButton: {
-        marginRight: 'auto'
-    }
-}
-class AppShell extends React.Component{
-    constructor(props){
+      marginRight: 'auto'
+    },
+};
+
+class AppShell extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            toggle:false
+        this.state = {
+            toggle: false
         };
     }
-    handleDrawerToggle =() => this.setState({toggle:!this.state.toggle})
-    render(){
+    handleDrawerToggle = () => this.setState({toggle: !this.state.toggle})
+    render() {
         const { classes } = this.props;
         return (
-            <div className={classes.root}>
-                <AppBar position = "static">
-                    <IconButton className ={classes.menuButton} color="inherit" onClick={}>
-                        <MenuIcon/>
-                    </IconButton>
-                </AppBar>
-                <Drawer open={this.state.toggle}>
-                    <MenuItem onClick={this.handleDrowerToggle}>Home</MenuItem>
-                    <MenuItem onClick={this.handleDrowerToggle}>Home 2</MenuItem>
-                </Drawer>
+            <div>
+                <div className={classes.root}>
+                    <AppBar position="static">
+                        <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
+                            <MenuIcon/>
+                        </IconButton>
+                    </AppBar>
+                    <Drawer open={this.state.toggle}>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/">
+                                Home
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/texts">
+                                Texts
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to="/words">
+                                Words
+                            </Link>
+                        </MenuItem>
+                    </Drawer>
+                </div>
+                <div id="content" style={{margin: 'auto', marginTop: '20px'}}>
+                    {React.cloneElement(this.props.children)}
+                </div>
             </div>
-        )
+        );
     }
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <Card>
+                <CardContent>
+                    Reat 및 블라블라 외치고 있는 어플리케이션
+                </CardContent>
+            </Card>
+        );
+    }
+}
+
+export default Home;
